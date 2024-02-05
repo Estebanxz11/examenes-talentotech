195278d7f4a78d43be3e710f27be4d70f3f315a8
miguel8888888 <86738300+miguel8888888@users.noreply.github.com>
2024-02-05 12:37:52 -0500

    config inicial

--- database/migrations/2021_07_21_140954_create_permission_tables.php
+++ database/migrations/2021_07_21_140954_create_permission_tables.php
@@ -22,10 +22,8 @@ class CreatePermissionTables extends Migration
 
         Schema::create($tableNames['permissions'], function (Blueprint $table) {
             $table->bigIncrements('id');
-            // $table->string('name');       // For MySQL 8.0 use string('name', 125);
-            // $table->string('guard_name'); // For MySQL 8.0 use string('guard_name', 125);
-            $table->string('name', 100)->unique();
-            $table->string('guard_name', 100);
+            $table->string('name');       // For MySQL 8.0 use string('name', 125);
+            $table->string('guard_name'); // For MySQL 8.0 use string('guard_name', 125);
             $table->timestamps();
 
             $table->unique(['name', 'guard_name']);
@@ -33,10 +31,8 @@ class CreatePermissionTables extends Migration
 
         Schema::create($tableNames['roles'], function (Blueprint $table) {
             $table->bigIncrements('id');
-            // $table->string('name');       // For MySQL 8.0 use string('name', 125);
-            // $table->string('guard_name'); // For MySQL 8.0 use string('guard_name', 125);
-            $table->string('name', 100)->unique();
-            $table->string('guard_name', 100);
+            $table->string('name');       // For MySQL 8.0 use string('name', 125);
+            $table->string('guard_name'); // For MySQL 8.0 use string('guard_name', 125);
             $table->timestamps();
 
             $table->unique(['name', 'guard_name']);
